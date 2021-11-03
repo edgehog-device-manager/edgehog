4c34c817a14254118a7c337b7c84321b6663296c
Riccardo Binetti <riccardo.binetti@secomind.com>
2021-11-03 19:32:33 +0100

    Populate basic seeds.exs for testing
    Signed-off-by: Riccardo Binetti <riccardo.binetti@secomind.com>

--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -1,11 +1,19 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Edgehog.Repo.insert!(%Edgehog.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Edgehog.{
+  Astarte,
+  Tenants
+}
+
+{:ok, cluster} =
+  Astarte.create_cluster(%{
+    name: "Test Cluster",
+    base_api_url: "https://api.astarte.example.com"
+  })
+
+{:ok, tenant} = Tenants.create_tenant(%{name: "ACME Inc"})
+
+_ = Edgehog.Repo.put_tenant_id(tenant.tenant_id)
+
+{:ok, realm} = Astarte.create_realm(cluster, %{name: "test", private_key: "notaprivatekey"})
+
+{:ok, _device} =
+  Astarte.create_device(realm, %{name: "Thingie", device_id: "DqL4H107S42WBEHmDrvPLQ"})
