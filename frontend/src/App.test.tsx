0cc6b835dcb1cf5173b8ec091a1c3db847f77f1c
Davide Briani <davide.briani@ispirata.com>
2021-11-03 11:45:01 +0100

    Remove unused default boilerplate
    Remove unused icons, css, and packages.
    
    Signed-off-by: Davide Briani <davide.briani@ispirata.com>

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders correctly", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const app = screen.getByTestId("app");
+  expect(app).toBeInTheDocument();
 });
