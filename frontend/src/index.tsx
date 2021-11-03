de4df232e804574c1412ef6c971d883c04523739
Davide Briani <davide.briani@ispirata.com>
2021-11-03 13:11:49 +0100

    Add i18n support for frontend
    - npm install @formatjs/cli react-intl
    - Add npm scripts to extract strings and compile translations
    - Setup a React Provider to provide translations for React components
    
    Signed-off-by: Davide Briani <davide.briani@ispirata.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import { RelayEnvironmentProvider } from "react-relay/hooks";
 
 import { relayEnvironment } from "api";
+import I18nProvider from "i18n";
 import App from "./App";
 import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <RelayEnvironmentProvider environment={relayEnvironment}>
-      <App />
+      <I18nProvider>
+        <App />
+      </I18nProvider>
     </RelayEnvironmentProvider>
   </React.StrictMode>,
   document.getElementById("root")
