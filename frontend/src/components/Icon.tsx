1f15e633a024418aed58854a589c70d39e679531
Riccardo Binetti <riccardo.binetti@secomind.com>
2021-11-09 17:00:52 +0100

    Fix license header
    s/SECO Mind/SECO Mind Srl/g
    
    Signed-off-by: Riccardo Binetti <riccardo.binetti@secomind.com>

--- frontend/src/components/Icon.tsx
+++ frontend/src/components/Icon.tsx
@@ -1,7 +1,7 @@
 /*
   This file is part of Edgehog.
 
-  Copyright 2021 SECO Mind
+  Copyright 2021 SECO Mind Srl
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
