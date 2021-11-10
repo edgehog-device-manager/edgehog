cff5847d096a86b7f08ab1414fb4306f4702ebd0
Davide Briani <davide.briani@ispirata.com>
2021-11-10 16:39:05 +0100

    Add forms and validation for Hardware Types
    - Add a form component for the creation of hardware types
    - Add a form component for the update of hardware types
    - Setup yup validation and translated error messages
    
    Signed-off-by: Davide Briani <davide.briani@ispirata.com>

---
+++ frontend/src/forms/index.ts
@@ -0,0 +1,55 @@
+/*
+  This file is part of Edgehog.
+
+  Copyright 2021 SECO Mind Srl
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+import * as yup from "yup";
+import { defineMessages } from "react-intl";
+
+const messages = defineMessages({
+  required: {
+    id: "validation.required",
+    defaultMessage: "Required.",
+  },
+  unique: {
+    id: "validation.unique",
+    defaultMessage: "Duplicate value.",
+  },
+  arrayMin: {
+    id: "validation.array.min",
+    defaultMessage: "Does not have enough values.",
+  },
+  partNumberFormat: {
+    id: "validation.partNumber.format",
+    defaultMessage:
+      "The part number must start with a letter and only contain lower case characters, numbers or the hyphen symbol -",
+  },
+});
+
+yup.setLocale({
+  mixed: {
+    required: messages.required.id,
+  },
+  array: {
+    min: messages.arrayMin.id,
+  },
+});
+
+const hardwareTypeHandleSchema = yup
+  .string()
+  .matches(/^[a-z][a-z\d-]*$/, messages.partNumberFormat.id);
+
+export { hardwareTypeHandleSchema, messages, yup };
