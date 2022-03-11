c167b9f32a3964b3dc33d847c0f576ca0caf1d5e
Mattia <mattia.pavinati@secomind.com>
2022-03-11 11:26:07 +0100

    Add SPDX-License-Identifier to committed files
    add SPDX-License-Identifier to the initial file comment
    or add it in a dedicated license file
    closes #155
    
    Signed-off-by: Mattia <mattia.pavinati@secomind.com>

--- frontend/set-backend-url.sh
+++ frontend/set-backend-url.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2021-2022 SECO Mind Srl
+# SPDX-License-Identifier: Apache-2.0
 
 sed -i 's|data-backend-url=\"[^\"]*\"|data-backend-url=\"'"$BACKEND_URL"'\"|' /usr/share/nginx/html/index.html
 
