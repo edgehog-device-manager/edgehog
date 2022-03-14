6e38392cd3c946e2c59ac9b3c0c7579196de62e9
Mattia Pavinati <mattia.pavinati@secomind.com>
2022-03-14 15:10:26 +0100

    Add frontend footer
    Add footer with the app name, version and usefull links
    such as the homepage, git repo and issue tracker
    closes #153

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,76 @@
+/*
+  This file is part of Edgehog.
+
+  Copyright 2022 SECO Mind Srl
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  SPDX-License-Identifier: Apache-2.0
+*/
+
+import assets from "assets";
+import Center from "components/Center";
+import Icon from "components/Icon";
+import Stack from "components/Stack";
+import "./Footer.scss";
+
+type FooterProps = {
+  appName: string;
+  appVersion: string;
+  homepageUrl: string;
+  repoUrl: string;
+  issueTrackerUrl: string;
+};
+
+const Footer = ({
+  appName,
+  appVersion,
+  homepageUrl,
+  repoUrl,
+  issueTrackerUrl,
+}: FooterProps) => {
+  return (
+    <footer className="pt-2 pb-1 border-top">
+      <Center>
+        <div>
+          <Stack gap={2} direction="horizontal">
+            <a href={homepageUrl}>
+              <img alt="Logo" src={assets.images.brand} />
+            </a>
+            <span>
+              {appName} <small>(v{appVersion})</small>
+            </span>
+            <a
+              href={repoUrl}
+              className="text-reset"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icon icon="github" />
+            </a>
+            <a
+              href={issueTrackerUrl}
+              className="text-reset"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Icon icon="bug" />
+            </a>
+          </Stack>
+        </div>
+      </Center>
+    </footer>
+  );
+};
+
+export default Footer;
