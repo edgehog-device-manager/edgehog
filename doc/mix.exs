7428009f09c3aed00126f790dc23520eea408f87
Davide Bettio <davide.bettio@ispirata.com>
2022-03-16 12:25:22 +0100

    doc: change glob to take only PNGs from assets
    .license files make documentation generation fail, so ignore them by
    using `*.png` glob instead of `*`.
    Fixes issue introduced with PR #165.

--- doc/mix.exs
+++ doc/mix.exs
@@ -44,7 +44,7 @@ defmodule Doc.MixProject do
       main: "intro_user",
       logo: "images/logo-favicon.png",
       extras: extras(),
-      assets: "images/",
+      assets: "images/*.png",
       api_reference: false,
       groups_for_extras: [
         "User Guide": ~r"/user/",
