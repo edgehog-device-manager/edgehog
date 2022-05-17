5c607328f26858c992d051b162b082799e0bc372
Riccardo Binetti <riccardo.binetti@secomind.com>
2022-05-17 19:50:09 +0200

    Add DeviceTag association schema
    Generated with:
    
    mix phx.gen.schema Devices.DeviceTag devices_tags tenant_id:references:tenants \
      tag_id:references:tags device_id:references:devices
    
    License header added after generation.
    
    Signed-off-by: Riccardo Binetti <riccardo.binetti@secomind.com>

---
+++ backend/priv/repo/migrations/20220517063737_create_devices_tags.exs
@@ -0,0 +1,37 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2022 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule Edgehog.Repo.Migrations.CreateDevicesTags do
+  use Ecto.Migration
+
+  def change do
+    create table(:devices_tags) do
+      add :tenant_id, references(:tenants, on_delete: :nothing)
+      add :tag_id, references(:tags, on_delete: :nothing)
+      add :device_id, references(:devices, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:devices_tags, [:tenant_id])
+    create index(:devices_tags, [:tag_id])
+    create index(:devices_tags, [:device_id])
+  end
+end
