fa33d57762a1e96e067d5a28b2278ab0825bd2ba
Davide Briani <davide.briani@secomind.com>
2022-06-07 11:18:02 +0200

    Activate default plugins for PromEx
    Select all relevant plugins to be used.
    
    Signed-off-by: Davide Briani <davide.briani@secomind.com>

--- backend/lib/edgehog/prom_ex.ex
+++ backend/lib/edgehog/prom_ex.ex
@@ -1,3 +1,23 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2022 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 defmodule Edgehog.PromEx do
   @moduledoc """
   Be sure to add the following to finish setting up PromEx:
@@ -61,12 +81,12 @@ defmodule Edgehog.PromEx do
     [
       # PromEx built in plugins
       Plugins.Application,
-      Plugins.Beam
-      # {Plugins.Phoenix, router: EdgehogWeb.Router, endpoint: EdgehogWeb.Endpoint},
-      # Plugins.Ecto,
+      Plugins.Beam,
+      {Plugins.Phoenix, router: EdgehogWeb.Router, endpoint: EdgehogWeb.Endpoint},
+      Plugins.Ecto,
       # Plugins.Oban,
       # Plugins.PhoenixLiveView,
-      # Plugins.Absinthe,
+      Plugins.Absinthe
       # Plugins.Broadway,
 
       # Add your own PromEx metrics plugins
@@ -87,12 +107,12 @@ defmodule Edgehog.PromEx do
     [
       # PromEx built in Grafana dashboards
       {:prom_ex, "application.json"},
-      {:prom_ex, "beam.json"}
-      # {:prom_ex, "phoenix.json"},
-      # {:prom_ex, "ecto.json"},
+      {:prom_ex, "beam.json"},
+      {:prom_ex, "phoenix.json"},
+      {:prom_ex, "ecto.json"},
       # {:prom_ex, "oban.json"},
       # {:prom_ex, "phoenix_live_view.json"},
-      # {:prom_ex, "absinthe.json"},
+      {:prom_ex, "absinthe.json"}
       # {:prom_ex, "broadway.json"},
 
       # Add your dashboard definitions here with the format: {:otp_app, "path_in_priv"}
