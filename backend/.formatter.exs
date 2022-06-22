f8d57b672551f49dc0b9a90ac81a571b4fcf4095
Riccardo Binetti <riccardo.binetti@secomind.com>
2022-06-22 09:32:55 +0200

    Add nimble_parsec to deps
    Also add it to the formatter plugins
    
    Signed-off-by: Riccardo Binetti <riccardo.binetti@secomind.com>

--- backend/.formatter.exs
+++ backend/.formatter.exs
@@ -1,8 +1,8 @@
-# SPDX-FileCopyrightText: 2021 SECO Mind Srl
+# SPDX-FileCopyrightText: 2021-2022 SECO Mind Srl
 # SPDX-License-Identifier: CC0-1.0
 
 [
-  import_deps: [:ecto, :phoenix, :absinthe, :skogsra],
+  import_deps: [:ecto, :phoenix, :absinthe, :skogsra, :nimble_parsec],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"]
 ]
