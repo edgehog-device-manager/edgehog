7d26dfaa4c58dba15145c20d0de217aa1953dfdc
Sergey Zakhlypa <sergey.zakhlypa@secomind.com>
2023-01-31 14:55:46 +0100

    Add table component to display Base Images
    Add the BaseImagesTable React component
    
    Signed-off-by: Sergey Zakhlypa <sergey.zakhlypa@secomind.com>

---
+++ frontend/src/components/BaseImagesTable.tsx
@@ -0,0 +1,106 @@
+/*
+  This file is part of Edgehog.
+
+  Copyright 2023 SECO Mind Srl
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  SPDX-License-Identifier: Apache-2.0
+*/
+
+import { FormattedMessage } from "react-intl";
+import { graphql, useFragment } from "react-relay";
+
+import type {
+  BaseImagesTable_BaseImagesFragment$data,
+  BaseImagesTable_BaseImagesFragment$key,
+} from "api/__generated__/BaseImagesTable_BaseImagesFragment.graphql";
+
+import Table from "components/Table";
+import type { Column } from "components/Table";
+
+// We use graphql fields below in columns configuration
+/* eslint-disable relay/unused-fields */
+const BASE_IMAGES_TABLE_FRAGMENT = graphql`
+  fragment BaseImagesTable_BaseImagesFragment on BaseImageCollection {
+    baseImages {
+      version
+      startingVersionRequirement
+      releaseDisplayName
+    }
+  }
+`;
+
+type TableRecord =
+  BaseImagesTable_BaseImagesFragment$data["baseImages"][number];
+
+const columns: Column<TableRecord>[] = [
+  {
+    accessor: "version",
+    Header: (
+      <FormattedMessage
+        id="components.BaseImagesTable.versionTitle"
+        defaultMessage="Base Image Version"
+        description="Title for the Version column of the base images table"
+      />
+    ),
+  },
+  {
+    accessor: "releaseDisplayName",
+    Header: (
+      <FormattedMessage
+        id="components.BaseImagesTable.releaseDisplayNameTitle"
+        defaultMessage="Release Name"
+        description="Title for the Release Name column of the base images table"
+      />
+    ),
+  },
+  {
+    accessor: "startingVersionRequirement",
+    Header: (
+      <FormattedMessage
+        id="components.BaseImagesTable.startingVersionRequirementTitle"
+        defaultMessage="Supported Starting Versions"
+        description="Title for the Supported Starting Versions column of the base images table"
+      />
+    ),
+  },
+];
+
+interface Props {
+  className?: string;
+  baseImageCollectionRef: BaseImagesTable_BaseImagesFragment$key;
+  hideSearch?: boolean;
+}
+
+const BaseImagesTable = ({
+  className,
+  baseImageCollectionRef,
+  hideSearch = false,
+}: Props) => {
+  const { baseImages } = useFragment(
+    BASE_IMAGES_TABLE_FRAGMENT,
+    baseImageCollectionRef
+  );
+
+  return (
+    <Table
+      className={className}
+      columns={columns}
+      data={baseImages}
+      hideSearch={hideSearch}
+    />
+  );
+};
+
+export default BaseImagesTable;
