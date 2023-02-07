88de5b9d2ac014359ab59b1a2d6df46e4f07e68a
Riccardo Binetti <riccardo.binetti@secomind.com>
2023-02-07 11:53:52 +0100

    GraphQL: add API to fetch and list UpdateChannel objects
    Also add relevant tests
    
    Signed-off-by: Riccardo Binetti <riccardo.binetti@secomind.com>

---
+++ backend/lib/edgehog_web/resolvers/update_campaigns.ex
@@ -0,0 +1,33 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2023 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule EdgehogWeb.Resolvers.UpdateCampaigns do
+  alias Edgehog.UpdateCampaigns
+
+  def find_update_channel(args, _resolution) do
+    UpdateCampaigns.fetch_update_channel(args.id)
+  end
+
+  def list_update_channels(_args, _resolution) do
+    update_channels = UpdateCampaigns.list_update_channels()
+
+    {:ok, update_channels}
+  end
+end
---
+++ backend/lib/edgehog_web/schema/update_campaigns_types.ex
@@ -0,0 +1,59 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2023 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule EdgehogWeb.Schema.UpdateCampaignsTypes do
+  use Absinthe.Schema.Notation
+  use Absinthe.Relay.Schema.Notation, :modern
+
+  alias EdgehogWeb.Resolvers
+
+  @desc """
+  Represents an UpdateChannel.
+
+  An UpdateChannel represents a set of TargetGroups that can be targeted in an \
+  UpdateCampaign
+  """
+  node object(:update_channel) do
+    @desc "The display name of the target group."
+    field :name, non_null(:string)
+
+    @desc "The identifier of the target group."
+    field :handle, non_null(:string)
+
+    @desc "The DeviceGroups associated with this UpdateChannel"
+    field :target_groups, non_null(list_of(non_null(:device_group)))
+  end
+
+  object :update_campaigns_queries do
+    @desc "Fetches the list of all update channels."
+    field :update_channels, non_null(list_of(non_null(:update_channel))) do
+      resolve &Resolvers.UpdateCampaigns.list_update_channels/2
+    end
+
+    @desc "Fetches a single update channel."
+    field :update_channel, :update_channel do
+      @desc "The ID of the update channel."
+      arg :id, non_null(:id)
+
+      middleware Absinthe.Relay.Node.ParseIDs, id: :update_channel
+      resolve &Resolvers.UpdateCampaigns.find_update_channel/2
+    end
+  end
+end
