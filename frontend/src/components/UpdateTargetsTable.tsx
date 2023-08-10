c8e205dc0f01490f6bf5d324430ba3eca025cb6b
Sergey Zakhlypa <sergey.zakhlypa@secomind.com>
2023-08-10 18:46:41 +0200

    Update UpdateTargetsTable with new GraphQL data
    Add columns for updateTarget's `Latest attempt`, `Completed at` data;
    otaOperation's `status`, `status progress` and `status code` data.
    
    Signed-off-by: Sergey Zakhlypa <sergey.zakhlypa@secomind.com>

--- frontend/src/components/UpdateTargetsTable.tsx
+++ frontend/src/components/UpdateTargetsTable.tsx
@@ -18,11 +18,13 @@
   SPDX-License-Identifier: Apache-2.0
 */
 
-import { defineMessages, FormattedMessage } from "react-intl";
+import { defineMessages, FormattedDate, FormattedMessage } from "react-intl";
 import { graphql, useFragment } from "react-relay/hooks";
 
 import type {
   UpdateTargetStatus,
+  OtaOperationStatus,
+  OtaOperationStatusCode,
   UpdateTargetsTable_UpdateTargetsFragment$data,
   UpdateTargetsTable_UpdateTargetsFragment$key,
 } from "api/__generated__/UpdateTargetsTable_UpdateTargetsFragment.graphql";
@@ -41,6 +43,13 @@ const UPDATE_TARGETS_TABLE_FRAGMENT = graphql`
         name
       }
       status
+      latestAttempt
+      completionTimestamp
+      otaOperation {
+        status
+        statusProgress
+        statusCode
+      }
     }
   }
 `;
@@ -80,6 +89,112 @@ const TargetStatus = ({ status }: { status: UpdateTargetStatus }) => (
   </div>
 );
 
+const getOperationStatusColor = (status: OtaOperationStatus): string => {
+  switch (status) {
+    case "PENDING":
+      return "text-muted";
+    case "SUCCESS":
+      return "text-success";
+    case "FAILURE":
+      return "text-danger";
+
+    case "ACKNOWLEDGED":
+    case "DEPLOYED":
+    case "DEPLOYING":
+    case "DOWNLOADING":
+    case "ERROR":
+    case "REBOOTING":
+      return "text-warning";
+  }
+};
+
+const operationStatusMessages = defineMessages<OtaOperationStatus>({
+  PENDING: {
+    id: "components.UpdateTargetsTable.otaOperationStatus.Pending",
+    defaultMessage: "Pending",
+  },
+  ACKNOWLEDGED: {
+    id: "components.UpdateTargetsTable.otaOperationStatus.Acknowledged",
+    defaultMessage: "Acknowledged",
+  },
+  DOWNLOADING: {
+    id: "components.UpdateTargetsTable.otaOperationStatus.Downloading",
+    defaultMessage: "Downloading",
+  },
+  DEPLOYING: {
+    id: "components.UpdateTargetsTable.otaOperationStatus.Deploying",
+    defaultMessage: "Deploying",
+  },
+  DEPLOYED: {
+    id: "components.UpdateTargetsTable.otaOperationStatus.Deployed",
+    defaultMessage: "Deployed",
+  },
+  REBOOTING: {
+    id: "components.UpdateTargetsTable.otaOperationStatus.Rebooting",
+    defaultMessage: "Rebooting",
+  },
+  SUCCESS: {
+    id: "components.UpdateTargetsTable.otaOperationStatus.Success",
+    defaultMessage: "Success",
+  },
+  ERROR: {
+    id: "components.UpdateTargetsTable.otaOperationStatus.Error",
+    defaultMessage: "Error",
+  },
+  FAILURE: {
+    id: "components.UpdateTargetsTable.otaOperationStatus.Failure",
+    defaultMessage: "Failure",
+  },
+});
+
+const OperationStatus = ({ status }: { status: OtaOperationStatus }) => (
+  <div className="d-flex align-items-center">
+    <Icon icon="circle" className={`me-2 ${getOperationStatusColor(status)}`} />
+    <span>
+      <FormattedMessage id={operationStatusMessages[status].id} />
+    </span>
+  </div>
+);
+
+const operationStatusCodeMessages = defineMessages<OtaOperationStatusCode>({
+  CANCELED: {
+    id: "components.UpdateTargetsTable.otaOperationStatusCode.Canceled",
+    defaultMessage: "Canceled",
+  },
+  INTERNAL_ERROR: {
+    id: "components.UpdateTargetsTable.otaOperationStatusCode.InternalError",
+    defaultMessage: "Internal error",
+  },
+  INVALID_BASE_IMAGE: {
+    id: "components.UpdateTargetsTable.otaOperationStatusCode.InvalidBaseImage",
+    defaultMessage: "Invalid Base Image",
+  },
+  INVALID_REQUEST: {
+    id: "components.UpdateTargetsTable.otaOperationStatusCode.InvalidRequest",
+    defaultMessage: "Invalid request",
+  },
+  IO_ERROR: {
+    id: "components.UpdateTargetsTable.otaOperationStatusCode.IOError",
+    defaultMessage: "IO error",
+  },
+  NETWORK_ERROR: {
+    id: "components.UpdateTargetsTable.otaOperationStatusCode.NetworkError",
+    defaultMessage: "Network error",
+  },
+  REQUEST_TIMEOUT: {
+    id: "components.UpdateTargetsTable.otaOperationStatusCode.RequestTimeout",
+    defaultMessage: "Request timeout",
+  },
+  SYSTEM_ROLLBACK: {
+    id: "components.UpdateTargetsTable.otaOperationStatusCode.SystemRollback",
+    defaultMessage: "System rollback",
+  },
+  UPDATE_ALREADY_IN_PROGRESS: {
+    id: "components.UpdateTargetsTable.otaOperationStatusCode.UpdateAlreadyInProgress",
+    defaultMessage: "Update already in progress",
+  },
+});
+
 type TableRecord =
   UpdateTargetsTable_UpdateTargetsFragment$data["updateTargets"][number];
 
@@ -111,6 +226,109 @@ const columns = [
       />
     ),
     cell: ({ getValue }) => <TargetStatus status={getValue()} />,
+  }),
+  columnHelper.accessor(
+    (updateTarget) => updateTarget.otaOperation?.status ?? null,
+    {
+      id: "otaOperationStatus",
+      header: () => (
+        <FormattedMessage
+          id="components.UpdateTargetsTable.otaOperationStatusTitle"
+          defaultMessage="Operation"
+          description="Title for the Operation Status column of the Update Targets table"
+        />
+      ),
+      cell: ({ getValue }) => {
+        const status = getValue();
+        return status && <OperationStatus status={status} />;
+      },
+    }
+  ),
+  columnHelper.accessor(
+    (updateTarget) => updateTarget.otaOperation?.statusProgress ?? null,
+    {
+      id: "otaOperationStatusProgress",
+      header: () => (
+        <FormattedMessage
+          id="components.UpdateTargetsTable.otaOperationStatusProgressTitle"
+          defaultMessage="Operation progress"
+          description="Title for the Operation Status Progress column of the Update Targets table"
+        />
+      ),
+      cell: ({ getValue }) => {
+        const progress = getValue();
+        return typeof progress === "number" ? `${progress}%` : "";
+      },
+    }
+  ),
+  columnHelper.accessor(
+    (updateTarget) => updateTarget.otaOperation?.statusCode ?? null,
+    {
+      id: "otaOperationStatusCode",
+      header: () => (
+        <FormattedMessage
+          id="components.UpdateTargetsTable.otaOperationStatusCodeTitle"
+          defaultMessage="Failure Reason"
+          description="Title for the Operation Status Code column of the Update Targets table"
+        />
+      ),
+      cell: ({ getValue }) => {
+        const statusCode = getValue();
+        return (
+          statusCode && (
+            <FormattedMessage id={operationStatusCodeMessages[statusCode].id} />
+          )
+        );
+      },
+    }
+  ),
+  columnHelper.accessor("latestAttempt", {
+    header: () => (
+      <FormattedMessage
+        id="components.UpdateTargetsTable.latestAttemptTitle"
+        defaultMessage="Latest attempt at"
+        description="Title for the Latest attempt at column of the Update Targets table"
+      />
+    ),
+    cell: ({ getValue }) => {
+      const latestAttempt = getValue();
+      return (
+        latestAttempt && (
+          <FormattedDate
+            value={latestAttempt}
+            year="numeric"
+            month="long"
+            day="numeric"
+            hour="numeric"
+            minute="numeric"
+          />
+        )
+      );
+    },
+  }),
+  columnHelper.accessor("completionTimestamp", {
+    header: () => (
+      <FormattedMessage
+        id="components.UpdateTargetsTable.completionTimestampTitle"
+        defaultMessage="Completed at"
+        description="Title for the Completed at column of the Update Targets table"
+      />
+    ),
+    cell: ({ getValue }) => {
+      const latestAttempt = getValue();
+      return (
+        latestAttempt && (
+          <FormattedDate
+            value={latestAttempt}
+            year="numeric"
+            month="long"
+            day="numeric"
+            hour="numeric"
+            minute="numeric"
+          />
+        )
+      );
+    },
   }),
 ];
 
