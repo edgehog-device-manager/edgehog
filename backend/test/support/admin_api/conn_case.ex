ed123a376c037da256a0e9ff3159b59b2cf17fa4
Sergey Zakhlypa <sergey.zakhlypa@secomind.com>
2023-10-24 05:11:17 +0200

    Add Admin REST API
    Add Admin REST API on `/admin-api/`.
    Expose tenant provisioning via POST to `/admin-api/v1/tenants`.
    
    Signed-off-by: Sergey Zakhlypa <sergey.zakhlypa@secomind.com>

---
+++ backend/test/support/admin_api/conn_case.ex
@@ -0,0 +1,62 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2023 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule EdgehogWeb.AdminAPI.ConnCase do
+  @moduledoc """
+  This module defines the test case to be used by
+  tests that require setting up a connection.
+
+  Such tests rely on `Phoenix.ConnTest` and also
+  import other functionality to make it easier
+  to build common data structures and query the data layer.
+
+  Finally, if the test case interacts with the database,
+  we enable the SQL sandbox, so changes done to the database
+  are reverted at the end of every test. If you are using
+  PostgreSQL, you can even run database tests asynchronously
+  by setting `use EdgehogWeb.ConnCase, async: true`, although
+  this option is not recommended for other databases.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      # Import conveniences for testing with connections
+      import Plug.Conn
+      import Phoenix.ConnTest
+      import EdgehogWeb.AdminAPI.ConnCase
+
+      alias EdgehogWeb.Router.Helpers, as: Routes
+
+      # The default endpoint for testing
+      @endpoint EdgehogWeb.Endpoint
+    end
+  end
+
+  setup tags do
+    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Edgehog.Repo, shared: not tags[:async])
+    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+
+    conn = Phoenix.ConnTest.build_conn()
+
+    {:ok, conn: conn}
+  end
+end
