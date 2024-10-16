19d5d0b02be823c82b7ab8b28ef10673949de275
Francesco Noacco <francesco.noacco@secomind.com>
2024-10-16 15:10:09 +0200

    chore(containers): init application resource
    - default actions
    - multitenant resource
    - unique name
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- backend/lib/edgehog/containers/application.ex
+++ backend/lib/edgehog/containers/application.ex
@@ -1,12 +1,30 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2024 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 defmodule Edgehog.Containers.Application do
   @moduledoc false
-  use Ash.Resource,
-    otp_app: :edgehog,
-    domain: Edgehog.Containers,
-    data_layer: AshPostgres.DataLayer
+  use Edgehog.MultitenantResource,
+    domain: Edgehog.Containers
 
   actions do
-    defaults [:read, :destroy, create: [], update: []]
+    defaults [:read, :destroy, create: [:name, :description], update: [:name, :description]]
   end
 
   attributes do
@@ -20,8 +38,11 @@ defmodule Edgehog.Containers.Application do
     timestamps()
   end
 
+  identities do
+    identity :name, [:name]
+  end
+
   postgres do
     table "applications"
-    repo Edgehog.Repo
   end
 end
