ab0b139d46f9a477e41a14d3f33c1581ac103323
Luca Zaninotto <luca.zaninotto@secomind.com>
2024-11-04 12:21:21 +0100

    chore(containers): generated volume resource
    ```
    mix ash.gen.resource Edgehog.Containers.Volume --uuid-primary-key id \
          --attribute driver:string:required \
          --attribute options:map \
          --timestamps \
          --extend postgres,graphql \
          --default-actions read,create,update,destroy
    ```
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

---
+++ backend/lib/edgehog/containers/volume.ex
@@ -0,0 +1,54 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2024 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule Edgehog.Containers.Volume do
+  @moduledoc false
+  use Edgehog.MultitenantResource,
+    domain: Edgehog.Containers
+
+  actions do
+    defaults [
+      :read,
+      :destroy,
+      create: [:driver, :options],
+      update: [:driver, :options]
+    ]
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :driver, :string do
+      default "local"
+      allow_nil? false
+    end
+
+    attribute :options, :map do
+      default %{}
+      allow_nil? false
+    end
+
+    timestamps()
+  end
+
+  postgres do
+    table "volumes"
+  end
+end
