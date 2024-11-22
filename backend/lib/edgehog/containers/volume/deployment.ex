b90997ad57f897a3bdcbe0415d43af2c3e9d38b6
Luca Zaninotto <luca.zaninotto@secomind.com>
2024-11-22 16:56:46 +0100

    chore(containers): init gengerated resources
    Setting up generated resources
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

--- backend/lib/edgehog/containers/volume/deployment.ex
+++ backend/lib/edgehog/containers/volume/deployment.ex
@@ -1,8 +1,32 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2024 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 defmodule Edgehog.Containers.Volume.Deployment do
   @moduledoc false
-  use Ash.Resource,
-    otp_app: :edgehog,
-    domain: Edgehog.Containers.Volume
+  use Edgehog.MultitenantResource,
+    domain: Edgehog.Containers,
+    extensions: [AshGraphql.Resource]
+
+  graphql do
+    type :volume_deployment
+  end
 
   actions do
     defaults [:read, :destroy, create: [:created], update: [:created]]
@@ -20,7 +44,19 @@ defmodule Edgehog.Containers.Volume.Deployment do
   end
 
   relationships do
-    belongs_to :container, Edgehog.Containers.Volume
+    belongs_to :volume, Edgehog.Containers.Volume do
+      attribute_type :uuid
+      public? true
+    end
+
     belongs_to :device, Edgehog.Devices.Device
+  end
+
+  identities do
+    identity :volume_instance, [:volume_id, :device_id]
+  end
+
+  postgres do
+    table "application_volume_deployments"
   end
 end
