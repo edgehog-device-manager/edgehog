b3fcd920da9ea66e745f62692b5c8f85bc0a5cfb
Omar <omar.brbutovic@secomind.com>
2025-08-15 10:31:56 +0200

    feat(containers): Add `add` and `drop` capabilities on containers
    Adds attributes to the containers, allowing for the specification
    of `add` and `drop` capabilities.
    
    Signed-off-by: Omar <omar.brbutovic@secomind.com>

--- backend/lib/edgehog/containers/container/container.ex
+++ backend/lib/edgehog/containers/container/container.ex
@@ -49,7 +49,9 @@ defmodule Edgehog.Containers.Container do
         :env,
         :privileged,
         :extra_hosts,
-        :image_id
+        :image_id,
+        :cap_add,
+        :cap_drop
       ],
       update: [
         :port_bindings,
@@ -59,7 +61,9 @@ defmodule Edgehog.Containers.Container do
         :env,
         :privileged,
         :extra_hosts,
-        :image_id
+        :image_id,
+        :cap_add,
+        :cap_drop
       ]
     ]
 
@@ -71,7 +75,9 @@ defmodule Edgehog.Containers.Container do
         :privileged,
         :port_bindings,
         :network_mode,
-        :extra_hosts
+        :extra_hosts,
+        :cap_add,
+        :cap_drop
       ]
 
       argument :image, :map
@@ -93,7 +99,9 @@ defmodule Edgehog.Containers.Container do
         :env,
         :privileged,
         :image_id,
-        :extra_hosts
+        :extra_hosts,
+        :cap_add,
+        :cap_drop
       ]
 
       argument :volumes, {:array, :map}
@@ -154,6 +162,18 @@ defmodule Edgehog.Containers.Container do
     end
 
     attribute :extra_hosts, {:array, :string} do
+      default []
+      allow_nil? false
+      public? true
+    end
+
+    attribute :cap_add, {:array, :string} do
+      default []
+      allow_nil? false
+      public? true
+    end
+
+    attribute :cap_drop, {:array, :string} do
       default []
       allow_nil? false
       public? true
