1df6ba2d6b88ee89dced2dc5fe351b594f6cc647
Damiano Mason <damiano.mason@secomind.com>
2025-09-10 09:24:49 +0200

    chore(containers): generate container_deployment-device_mapping_deployment resource
    many-to-many between a container deployment and the related device mapping deployments
    ```
    mix ash.gen.resource Edgehog.Containers.ContainerDeploymentDeviceMappingDeployment \
    --default-actions read,create,destroy \
    --relationship belongs_to:container_deployment:Edgehog.Containers.Container.Deployment:primary_key:required \
    --relationship belongs_to:device_mapping_deployment:Edgehog.Containers.DeviceMapping.Deployment:primary_key:required \
    --extend postgres
    ```
    
    Signed-off-by: Damiano Mason <damiano.mason@secomind.com>

---
+++ backend/lib/edgehog/containers/container_deployment_device_mapping_deployment.ex
@@ -0,0 +1,48 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2025 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule Edgehog.Containers.ContainerDeploymentDeviceMappingDeployment do
+  @moduledoc false
+  use Ash.Resource,
+    otp_app: :edgehog,
+    domain: Edgehog.Containers,
+    data_layer: AshPostgres.DataLayer
+
+  actions do
+    defaults [:read, :destroy, create: []]
+  end
+
+  relationships do
+    belongs_to :container_deployment, Edgehog.Containers.Container.Deployment do
+      primary_key? true
+      allow_nil? false
+    end
+
+    belongs_to :device_mapping_deployment, Edgehog.Containers.DeviceMapping.Deployment do
+      primary_key? true
+      allow_nil? false
+    end
+  end
+
+  postgres do
+    table "container_deployment_device_mapping_deployments"
+    repo Edgehog.Repo
+  end
+end
