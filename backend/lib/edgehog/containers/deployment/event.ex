edgehog-device-manager/edgehog

commit 7753a773a1da97c22f161c379b76cf784c1d2d79

refactor!: events are treated as events (#953)
We were using the deployment `state` to record two things at once:
- deployment state (:started, :stopped)
- deployment events (an error, starting, stopping, upgrading, etc.)

This change decouples these int their own fields: the deployment `state`
correctly reports the state, while a new `events` relationship displays the
events of a deployment.

BREAKING: deployment states change: now a deployment can only be in states `pending`, `sent`, `started` or `stopped`.

Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

backend/lib/edgehog/containers/deployment/event.ex
@@ -1,0 +1,69 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2025 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule Edgehog.Containers.Deployment.Event do
+  @moduledoc false
+  use Edgehog.MultitenantResource,
+    domain: Edgehog.Containers,
+    extensions: [AshGraphql.Resource]
+
+  alias Edgehog.Containers.Deployment.EventType
+
+  graphql do
+    type :deployment_event
+  end
+
+  actions do
+    defaults [
+      :read,
+      create: [:deployment_id, :type, :message]
+    ]
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :type, EventType do
+      public? true
+      allow_nil? false
+    end
+
+    attribute :message, :string do
+      public? true
+    end
+
+    timestamps()
+  end
+
+  relationships do
+    belongs_to :deployment, Edgehog.Containers.Deployment do
+      attribute_type :uuid
+      allow_nil? false
+    end
+  end
+
+  postgres do
+    table "deployment_events"
+
+    references do
+      reference :deployment, on_delete: :delete
+    end
+  end
+end

backend/lib/edgehog/containers/containers.ex
@@ -211,15 +211,15 @@ defmodule Edgehog.Containers do
       define :run_ready_actions, action: :run_ready_actions
       define :set_deployment_state, action: :set_state
       define :send_deployment, action: :send_deployment
+      define :append_deployment_event, action: :append_event
 
       define :mark_deployment_as_sent, action: :mark_as_sent
-      define :mark_deployment_as_deleting, action: :mark_as_deleting
-      define :mark_deployment_as_errored, action: :mark_as_errored, args: [:message]
       define :mark_deployment_as_started, action: :mark_as_started
-      define :mark_deployment_as_starting, action: :mark_as_starting
       define :mark_deployment_as_stopped, action: :mark_as_stopped
-      define :mark_deployment_as_stopping, action: :mark_as_stopping
+      define :mark_deployment_as_timed_out, action: :mark_as_timed_out
     end
+
+    resource Edgehog.Containers.Deployment.Event
 
     resource Edgehog.Containers.Image do
       define :fetch_image, action: :read, get_by: [:id]

backend/lib/edgehog/containers/deployment.ex
@@ -39,14 +39,14 @@ defmodule Edgehog.Containers.Deployment do
   graphql do
     type :deployment
 
-    paginate_relationship_with container_deployments: :relay
+    paginate_relationship_with container_deployments: :relay, events: :relay
   end
 
   actions do
     defaults [
       :read,
       :destroy,
-      create: [:device_id, :release_id, :state, :last_error_message]
+      create: [:device_id, :release_id, :state]
     ]
 
     create :deploy do
@@ -182,39 +182,26 @@ defmodule Edgehog.Containers.Deployment do
       require_atomic? false
     end
 
-    update :mark_as_starting do
-      require_atomic? false
-
-      change Changes.MarkAsStarting
-    end
-
     update :mark_as_stopped do
       change set_attribute(:state, :stopped)
 
       require_atomic? false
     end
 
-    update :mark_as_stopping do
-      require_atomic? false
+    update :mark_as_timed_out do
+      change set_attribute(:timed_out, true)
 
-      change Changes.MarkAsStopping
+      require_atomic? false
     end
 
-    update :mark_as_errored do
-      argument :message, :string do
+    update :append_event do
+      require_atomic? false
+
+      argument :event, :map do
         allow_nil? false
       end
 
-      change set_attribute(:last_error_message, arg(:message))
-      change set_attribute(:state, :error)
-
-      require_atomic? false
-    end
-
-    update :mark_as_deleting do
-      change set_attribute(:state, :deleting)
-
-      require_atomic? false
+      change Changes.AppendEvent
     end
 
     update :maybe_run_ready_actions do
@@ -244,7 +231,9 @@ defmodule Edgehog.Containers.Deployment do
       public? true
     end
 
-    attribute :last_error_message, :string do
+    attribute :timed_out, :boolean do
+      allow_nil? false
+      default false
       public? true
     end
 
@@ -271,6 +260,10 @@ defmodule Edgehog.Containers.Deployment do
       destination_attribute_on_join_resource :container_deployment_id
       public? true
     end
+
+    has_many :events, Edgehog.Containers.Deployment.Event do
+      public? true
+    end
   end
 
   calculations do
@@ -292,11 +285,9 @@ defmodule Edgehog.Containers.Deployment do
 
     publish :mark_as_sent, [[:id, "*"]]
     publish :mark_as_started, [[:id, "*"]]
-    publish :mark_as_starting, [[:id, "*"]]
     publish :mark_as_stopped, [[:id, "*"]]
-    publish :mark_as_stopping, [[:id, "*"]]
-    publish :mark_as_errored, [[:id, "*"]]
-    publish :mark_as_deleting, [[:id, "*"]]
+    publish :mark_as_timed_out, [[:id, "*"]]
+    publish :append_event, [[:id, "*"]]
     publish :maybe_run_ready_actions, [[:id, "*"]]
 
     transform fn notification ->
@@ -314,16 +305,16 @@ defmodule Edgehog.Containers.Deployment do
           action in [
             :mark_as_sent,
             :mark_as_started,
-            :mark_as_starting,
             :mark_as_stopped,
-            :mark_as_stopping,
-            :mark_as_deleting,
+            :append_event,
             :maybe_run_ready_actions
           ] ->
             :deployment_updated
 
-          action == :mark_as_errored ->
-            :deployment_error
+          action in [
+            :mark_as_timed_out
+          ] ->
+            :deployment_timeout
 
           true ->
             :unknown_event

backend/lib/edgehog/containers/deployment/calculations/ready.ex
@@ -30,7 +30,7 @@ defmodule Edgehog.Containers.Deployment.Calculations.Ready do
 
   alias Ash.Resource.Calculation
 
-  @ready_states [:started, :starting, :stopped, :stopping]
+  @ready_states [:started, :stopped]
 
   @impl Calculation
   def load(_query, _opts, _context), do: [state: [], container_deployments: :is_ready]

backend/lib/edgehog/containers/deployment/changes/append_event.ex
@@ -18,16 +18,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-defmodule Edgehog.Containers.Deployment.Types.DeploymentState do
-  @moduledoc false
+defmodule Edgehog.Containers.Deployment.Changes.AppendEvent do
+  @moduledoc """
+  Appends an event with the given argument details.
+  """
 
-  use Ash.Type.Enum,
-    values: [
-      pending: "The deployment has been created in the database layer, the device yet has to receive it.",
-      sent: "The deployment description has been sent to the device.",
-      started: "The deployment is started on the device.",
-      stopped: "The deployment is stopped on the device."
-    ]
+  use Ash.Resource.Change
 
-  def graphql_type(_), do: :application_deployment_state
+  @impl Ash.Resource.Change
+  def change(changeset, _opts, _context) do
+    deployment = changeset.data
+
+    event =
+      changeset
+      |> Ash.Changeset.get_argument(:event)
+      |> Map.put(:deployment_id, deployment.id)
+
+    Ash.Changeset.manage_relationship(changeset, :events, event, type: :create)
+  end
 end

backend/lib/edgehog/containers/deployment/event_type.ex
@@ -18,16 +18,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-defmodule Edgehog.Containers.Deployment.Types.DeploymentState do
-  @moduledoc false
+defmodule Edgehog.Containers.Deployment.EventType do
+  @moduledoc """
+  Expresses the type of event a device can communicate to the backend trough the `DeploymentEvent` interface.
+  """
 
   use Ash.Type.Enum,
     values: [
-      pending: "The deployment has been created in the database layer, the device yet has to receive it.",
-      sent: "The deployment description has been sent to the device.",
-      started: "The deployment is started on the device.",
-      stopped: "The deployment is stopped on the device."
+      starting: "The deployment is starting.",
+      stopping: "The deployment is stopping.",
+      started: "The deployment is started.",
+      stopped: "The deployment is stopped.",
+      updating: "The deployment is getting updated.",
+      deleting: "The deployment is getting deleted.",
+      error: "The deployment encountered an error."
     ]
 
-  def graphql_type(_), do: :application_deployment_state
+  def graphql_type(_), do: :deployment_event_type
 end

backend/lib/edgehog/deployment_campaigns/deployment_mechanism/lazy/core.ex
@@ -23,10 +23,13 @@ defmodule Edgehog.DeploymentCampaigns.DeploymentMechanism.Lazy.Core do
   Lazy executor core pure funcitons.
   """
   alias Edgehog.Containers
+  alias Edgehog.Containers.Deployment
   alias Edgehog.DeploymentCampaigns
   alias Edgehog.DeploymentCampaigns.DeploymentCampaign
   alias Edgehog.DeploymentCampaigns.DeploymentTarget
   alias Edgehog.Error.AstarteAPIError
+
+  require Ash.Query
 
   @doc """
   Fetches a deployment campaign by its ID and tenant ID, raising an error if not found.
@@ -170,7 +173,7 @@ defmodule Edgehog.DeploymentCampaigns.DeploymentMechanism.Lazy.Core do
 
   def mark_deployment_as_timed_out!(tenant_id, deployment) do
     # TODO: add timneout information on the deplyment and correctly handle this case
-    Containers.mark_deployment_as_errored!(deployment, "timed out.", tenant: tenant_id)
+    Containers.mark_deployment_as_timed_out!(deployment, tenant: tenant_id)
   end
 
   @doc """
@@ -491,6 +494,14 @@ defmodule Edgehog.DeploymentCampaigns.DeploymentMechanism.Lazy.Core do
         {:ok, target}
       end
     end
+  end
+
+  def get_latest_error_for_deployment!(tenant_id, deployment_id) do
+    Deployment.Event
+    |> Ash.Query.filter(deployment_id == ^deployment_id)
+    |> Ash.Query.filter(type == :error)
+    |> Ash.Query.sort(inserted_at: :desc)
+    |> Ash.read_first!(tenant: tenant_id)
   end
 
   defp do_deploy(target, release, _deployment_mechanism) do

backend/lib/edgehog/deployment_campaigns/deployment_mechanism/lazy/executor.ex
@@ -465,12 +465,13 @@ defmodule Edgehog.DeploymentCampaigns.DeploymentMechanism.Lazy.Executor do
     {:keep_state_and_data, actions}
   end
 
-  # Ignore deployment_updated events
+  # Ignore deployment_updated events, when everything will be ready the resource
+  # will emit a `:deployment_ready` event
   def handle_event(:info, %{payload: {:deployment_updated, _deployment}}, _state, _data) do
     :keep_state_and_data
   end
 
-  def handle_event(:info, %{payload: {:deployment_error, deployment}}, _state, data) do
+  def handle_event(:info, %{payload: {:deployment_timeout, deployment}}, _state, data) do
     # We always cancel the retry timeout for every kind of update we see on an Deployment.
     # This ensures we don't resend the request even if we accidentally miss the acknowledge.
     # If the timeout does not exist, this is a no-op anyway.
@@ -497,10 +498,16 @@ defmodule Edgehog.DeploymentCampaigns.DeploymentMechanism.Lazy.Executor do
   end
 
   def handle_event(:internal, {:deployment_failure, deployment}, state, data) do
+    latest_error_message =
+      case Core.get_latest_error_for_deployment!(deployment.tenant_id, deployment.id) do
+        %{message: message} -> message
+        nil -> "Could not find any error event."
+      end
+
     log_operation_failure(
       data.operation_type,
       deployment.device_id,
-      deployment.last_error_message
+      latest_error_message
     )
 
     _ =
@@ -589,8 +596,6 @@ defmodule Edgehog.DeploymentCampaigns.DeploymentMechanism.Lazy.Executor do
   def handle_event(:internal, {:retry_threshold_exceeded, target}, _state, data) do
     Logger.notice("Device #{target.device_id} update failed: no more retries left")
 
-    # Just mark the Deployment as failed with request_timeout. The associated target will
-    # be marked as failed when it receives the :deployment_updated message from the PubSub
     _ = Core.mark_deployment_as_timed_out!(data.tenant_id, target.deployment_id)
 
     :keep_state_and_data

commit 70f64859a3709ddbe05495ce2ae5a6426de4abd2

fix (frontend): fix broken UI
- Restored UI functionality broken by PR #953

Signed-off-by: ArnelaL <arnela.lisic@secomind.com>

backend/lib/edgehog/containers/deployment/event.ex
@@ -49,7 +49,9 @@ defmodule Edgehog.Containers.Deployment.Event do
       public? true
     end
 
-    timestamps()
+    timestamps do
+      public? true
+    end
   end
 
   relationships do

frontend/src/components/DeploymentState.tsx
@@ -45,16 +45,10 @@ const parseDeploymentState = (
       return "SENT";
     case "STARTED":
       return "STARTED";
-    case "STARTING":
-      return "STARTING";
+
     case "STOPPED":
       return "STOPPED";
-    case "STOPPING":
-      return "STOPPING";
-    case "ERROR":
-      return "ERROR";
-    case "DELETING":
-      return "DELETING";
+
     default:
       return "DEPLOYING";
   }

frontend/src/components/DeploymentTargetsTable.tsx
@@ -46,7 +46,19 @@ const DEPLOYMENT_TARGETS_TABLE_FRAGMENT = graphql`
     deployment {
       state
       isReady
-      lastErrorMessage
+      events(
+        filter: { type: { eq: ERROR } }
+        sort: [{ field: INSERTED_AT, order: DESC }]
+        first: 1
+      ) {
+        edges {
+          node {
+            message
+            type
+            insertedAt
+          }
+        }
+      }
     }
   }
 `;
@@ -121,7 +133,8 @@ const columns = [
     },
   ),
   columnHelper.accessor(
-    (deploymentTarget) => deploymentTarget.deployment?.lastErrorMessage ?? null,
+    (deploymentTarget) =>
+      deploymentTarget.deployment?.events?.edges?.[0]?.node?.message ?? null,
     {
       id: "lastErrorMessage",
       header: () => (
@@ -205,7 +218,6 @@ const DeploymentTargetsTable = ({
     DEPLOYMENT_TARGETS_TABLE_FRAGMENT,
     deploymentTargetsRef,
   );
-
   return (
     <InfiniteTable
       className={className}