d12881ba84c599d0544708869358e25bc9e32a76
Omar <omar.brbutovic@secomind.com>
2025-11-10 16:15:11 +0100

    refactor: add load callbacks to Container calculations for optimization
    Add `load/3` callbacks to calculations to specify which attributes
    are needed, improving query efficiency by only loading required fields
    
    Signed-off-by: Omar <omar.brbutovic@secomind.com>

--- backend/lib/edgehog/containers/image_credentials/base64_json_encoding.ex
+++ backend/lib/edgehog/containers/image_credentials/base64_json_encoding.ex
@@ -1,7 +1,7 @@
 #
 # This file is part of Edgehog.
 #
-# Copyright 2024 SECO Mind Srl
+# Copyright 2024 - 2025 SECO Mind Srl
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,9 +22,13 @@ defmodule Edgehog.Containers.ImageCredentials.Base64JsonEncoding do
   @moduledoc false
   use Ash.Resource.Calculation
 
+  alias Ash.Resource.Calculation
   alias AshGraphql.Types.JSONString
 
-  @impl Ash.Resource.Calculation
+  @impl Calculation
+  def load(_query, _opts, _context), do: [:username, :password]
+
+  @impl Calculation
   def calculate(records, _opts, _context) do
     for record <- records do
       %{username: record.username, password: record.password}
