a6a917c806d7a5f7e9f677757bbeae32e49429bb
Luca Zaninotto <luca.zaninotto@secomind.com>
2025-11-17 15:32:53 +0100

    chore: Init image credentials creation e2e test
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

--- tools/e2e_test/src/suite/mod.rs
+++ tools/e2e_test/src/suite/mod.rs
@@ -16,16 +16,5 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-mod image_credentials;
-mod suite;
-
-use clap::Parser;
-use suite::client::EdgehogClient;
-use suite::config::Config;
-
-#[tokio::main]
-async fn main() -> eyre::Result<()> {
-    let config = Config::parse();
-    let client = EdgehogClient::create(&config)?;
-    image_credentials::run_test(client).await
-}
+pub mod client;
+pub mod config;
