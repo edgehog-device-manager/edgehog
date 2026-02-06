1ab485038c0f22eb2c0dab13dc9a058f44adf13f
ArnelaL <arnela.lisic@secomind.com>
2026-02-06 09:10:59 +0100

    refactor(frontend): Restructure ImageCredentials
    - Refactor ImageCredentialsTable into a stateless, presentational component
    - Move Relay pagination, search, and filtering logic to
      ImageCredentials page
    
    Signed-off-by: ArnelaL <arnela.lisic@secomind.com>

--- frontend/src/components/ImageCredentialsTable.tsx
+++ frontend/src/components/ImageCredentialsTable.tsx
@@ -18,45 +18,36 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import _ from "lodash";
+import { useMemo } from "react";
 import { FormattedMessage } from "react-intl";
-import { graphql, usePaginationFragment } from "react-relay/hooks";
+import { graphql, useFragment } from "react-relay/hooks";
 
-import type { ImageCredentialsTable_PaginationQuery } from "@/api/__generated__/ImageCredentialsTable_PaginationQuery.graphql";
 import type {
-  ImageCredentialsTable_imageCredentials_Fragment$key,
-  ImageCredentialsTable_imageCredentials_Fragment$data,
-} from "@/api/__generated__/ImageCredentialsTable_imageCredentials_Fragment.graphql";
+  ImageCredentialsTable_ImageCredentialEdgeFragment$data,
+  ImageCredentialsTable_ImageCredentialEdgeFragment$key,
+} from "@/api/__generated__/ImageCredentialsTable_ImageCredentialEdgeFragment.graphql";
 
 import { createColumnHelper } from "@/components/Table";
 import { Link, Route } from "@/Navigation";
-import { useCallback, useEffect, useMemo, useState } from "react";
 import InfiniteTable from "./InfiniteTable";
-import _ from "lodash";
-import { RECORDS_TO_LOAD_FIRST, RECORDS_TO_LOAD_NEXT } from "@/constants";
 
 // We use graphql fields below in columns configuration
 /* eslint-disable relay/unused-fields */
-const IMAGE_CREDENTIALS_FRAGMENT = graphql`
-  fragment ImageCredentialsTable_imageCredentials_Fragment on RootQueryType
-  @refetchable(queryName: "ImageCredentialsTable_PaginationQuery")
-  @argumentDefinitions(filter: { type: "ImageCredentialsFilterInput" }) {
-    listImageCredentials(first: $first, after: $after, filter: $filter)
-      @connection(key: "ImageCredentialsTable_listImageCredentials") {
-      edges {
-        node {
-          id
-          label
-          username
-        }
+const IMAGE_CREDENTIALS_TABLE_FRAGMENT = graphql`
+  fragment ImageCredentialsTable_ImageCredentialEdgeFragment on ImageCredentialsConnection {
+    edges {
+      node {
+        id
+        label
+        username
       }
     }
   }
 `;
 
 type TableRecord = NonNullable<
-  NonNullable<
-    ImageCredentialsTable_imageCredentials_Fragment$data["listImageCredentials"]
-  >["edges"]
+  NonNullable<ImageCredentialsTable_ImageCredentialEdgeFragment$data>["edges"]
 >[number]["node"];
 
 const columnHelper = createColumnHelper<TableRecord>();
@@ -92,81 +83,35 @@ const columns = [
 
 type ImageCredentialsTableProps = {
   className?: string;
-  listImageCredentialsRef: ImageCredentialsTable_imageCredentials_Fragment$key;
-  hideSearch?: boolean;
+  imageCredentialsRef: ImageCredentialsTable_ImageCredentialEdgeFragment$key;
+  loading?: boolean;
+  onLoadMore?: () => void;
 };
 
 const ImageCredentialsTable = ({
   className,
-  listImageCredentialsRef,
-  hideSearch = false,
+  imageCredentialsRef,
+  loading = false,
+  onLoadMore,
 }: ImageCredentialsTableProps) => {
-  const { data, loadNext, hasNext, isLoadingNext, refetch } =
-    usePaginationFragment<
-      ImageCredentialsTable_PaginationQuery,
-      ImageCredentialsTable_imageCredentials_Fragment$key
-    >(IMAGE_CREDENTIALS_FRAGMENT, listImageCredentialsRef);
-
-  const [searchText, setSearchText] = useState<string | null>(null);
-
-  const debounceRefetch = useMemo(
-    () =>
-      _.debounce((text: string) => {
-        if (text === "") {
-          refetch(
-            {
-              first: RECORDS_TO_LOAD_FIRST,
-            },
-            { fetchPolicy: "network-only" },
-          );
-        } else {
-          refetch(
-            {
-              first: RECORDS_TO_LOAD_FIRST,
-              filter: {
-                or: [
-                  { label: { ilike: `%${text}%` } },
-                  { username: { ilike: `%${text}%` } },
-                ],
-              },
-            },
-            { fetchPolicy: "network-only" },
-          );
-        }
-      }, 500),
-    [refetch],
+  const imageCredentialsFragment = useFragment(
+    IMAGE_CREDENTIALS_TABLE_FRAGMENT,
+    imageCredentialsRef || null,
   );
 
-  useEffect(() => {
-    if (searchText !== null) {
-      debounceRefetch(searchText);
-    }
-  }, [debounceRefetch, searchText]);
-
-  const loadNextImageCredentials = useCallback(() => {
-    if (hasNext && !isLoadingNext) loadNext(RECORDS_TO_LOAD_NEXT);
-  }, [hasNext, isLoadingNext, loadNext]);
-
-  const imageCredentials: TableRecord[] = useMemo(() => {
+  const imageCredentials = useMemo<TableRecord[]>(() => {
     return (
-      data.listImageCredentials?.edges
-        ?.map((edge) => edge?.node)
-        .filter(
-          (node): node is TableRecord => node !== undefined && node !== null,
-        ) ?? []
+      _.compact(imageCredentialsFragment?.edges?.map((e) => e?.node)) ?? []
     );
-  }, [data]);
-
-  if (!data.listImageCredentials) return null;
-
+  }, [imageCredentialsFragment]);
   return (
     <InfiniteTable
       className={className}
       columns={columns}
       data={imageCredentials}
-      loading={isLoadingNext}
-      onLoadMore={hasNext ? loadNextImageCredentials : undefined}
-      setSearchText={hideSearch ? undefined : setSearchText}
+      loading={loading}
+      onLoadMore={onLoadMore}
+      hideSearch
     />
   );
 };
