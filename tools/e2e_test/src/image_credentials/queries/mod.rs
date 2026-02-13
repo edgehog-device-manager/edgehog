7b353564665203a2ef637ee6669c12fab9289e7a
Osman Hadzic <osman.hadzic@secomind.com>
2026-02-13 09:23:07 +0100

    fix: update copyright year
    update copyright year in various files to 2025 - 2026
    
    Signed-off-by: Osman Hadzic <osman.hadzic@secomind.com>

--- tools/e2e_test/src/image_credentials/queries/mod.rs
+++ tools/e2e_test/src/image_credentials/queries/mod.rs
@@ -1,6 +1,6 @@
 // This file is part of Edgehog.
 //
-// Copyright 2025 SECO Mind Srl
+// Copyright 2025 - 2026 SECO Mind Srl
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
