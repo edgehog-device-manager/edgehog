6d85ea655908a0e011b1c714ab3f5dce213ba0c3
Luca Zaninotto <luca.zaninotto@secomind.com>
2026-06-18 14:34:40 +0200

    fix: log only once
    Logging on state transitions is not as straightforward as ash wants you to
    think. Fixed with ad-hoc changes
    
    Signed-off-by: Luca Zaninotto <luca.zaninotto@secomind.com>

---
+++ backend/lib/edgehog/containers/deployment/changes/log_stopped.ex
@@ -0,0 +1,49 @@
+#
+# This file is part of Edgehog.
+#
+# Copyright 2026 SECO Mind Srl
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+defmodule Edgehog.Containers.Deployment.Changes.LogStopped do
+  @moduledoc """
+  Logs a stopped container status.
+  """
+
+  use Ash.Resource.Change
+
+  require Logger
+
+  @impl Ash.Resource.Change
+  def change(changeset, _opts, _context) do
+    context = Ash.Changeset.get_data(changeset, :context)
+    state = Ash.Changeset.get_data(changeset, :state)
+
+    metadata = [
+      resource: Edgehog.Containers.Deployment,
+      action: :mark_as_stopped
+    ]
+
+    case {context, state} do
+      {nil, :sent} -> Logger.info("Deployment successfully provisioned.", metadata)
+      {:stop_message_sent, _} -> Logger.info("Deployment successfully stopped.", metadata)
+      {:upgrade_message_sent, _} -> Logger.info("Deployment successfully upgraded.", metadata)
+      _ -> :ok
+    end
+
+    changeset
+  end
+end
